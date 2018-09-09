d5698564eea2b33906038f2c8f0b9d8d94113f30
Nayiem Willems <37852059+NayiemWillems@users.noreply.github.com>
2018-09-09 19:36:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -259,7 +259,7 @@ app.directive('wallet', ($rootScope, $timeout, wallet) => {
             toScreenshot.scrollTop(scrollPosition);
             var link = document.createElement('a');
             link.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-            link.download = 'ark-paperwallet.jpg';
+            link.download = 'qredit-paperwallet.jpg';
             link.click();
           }
         });
